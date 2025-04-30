54688edbc7f2698bf3453ac7404b94840cf45095
Emílio Nicoletti <eamnicoletti@gmail.com>
2025-04-30 14:52:00 -0300

    :wrench: chore: updating tailwing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
