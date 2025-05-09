c1b5b4f2ddfd4f4c1c5120a85320393b5405fa07
Emílio Nicoletti <eamnicoletti@gmail.com>
2025-05-09 19:37:29 -0300

    :wrench: chore: config production ORIGIN_URL

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// Lista de rotas protegidas
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
   '/summaries(.*)',
@@ -8,21 +7,14 @@ const isProtectedRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
-    await auth.protect()
-  }
+  if (isProtectedRoute(req)) await auth.protect()
 })
 
-// Configuração recomendada para App Router
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// }
--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  reactComponentAnnotation: {
-    enabled: process.env.NODE_ENV === 'development',
-  },
 }
 
 export default nextConfig
